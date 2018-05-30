52b6afe967bb38d0c26d079ac13dd932bb8a38b7
Johnny <john.gardner2010@gmail.com>
2018-05-29 20:26:47 -0400

    new changes!

---
+++ commands.py
@@ -0,0 +1,114 @@
+minecraft_commands_dict = {}
+discord_commands_dict = {}
+both_commands_dict = {}
+
+def minecraft_only(func):
+    minecraft_commands_dict[func.__name__] = func
+    return func
+
+def discord_only(func):
+    discord_commands_dict[func.__name__] = func
+    return func
+
+def both(func):
+    both_commands_dict[func.__name__] = func
+    return func
+
+async def discord_commands(args, client, message):
+    try:
+        if args[0][1:] in discord_commands_dict.keys():
+            await discord_commands_dict[args[0][1:]](args, client, message)
+    except Exception as e:
+        pass
+
+async def minecraft_commands(args, client, message):
+    try:
+        if args[0][1:] in minecraft_commands_dict.keys():
+            await minecraft_commands_dict[args[0][1:]](args, client, message)
+    except Exception as e:
+        pass
+
+async def both_commands(args, client, message):
+    try:
+        if args[0][1:] in both_commands_dict.keys():
+            await both_commands_dict[args[0][1:]](args, client, message)
+    except Exception as e:
+        pass
+
+@both
+async def discord(args, client, message):
+    await client.send_message(message.channel, "Join the discord: https://discord.gg/UBCqM8y")
+
+@discord_only
+async def log(args, client, message):
+    await client.send_message(
+        message.channel,
+        "Thanks, {} your message has been logged.".format(message.author)
+    )
+    await client.send_message(
+        client.get_channel('442476383113969664'),
+        str(message.author) + ' ' + ' '.join(args[1:])
+    )
+
+@both
+async def server(args, client, message):
+    await client.send_message(message.channel, "MS3D: ms3d.tahgcraft.com 3.3.1.4")
+    await client.send_message(message.channel, "Sevtech: sevtech.tahgcraft.com 3.0.7 - use FTBU x.14")
+    await client.send_message(message.channel, "Continuum: continuum.tahgcraft.com 1.0.2")
+
+@minecraft_only
+async def giveitem(args, client, message):
+    await client.send_message(message.channel, "No cheating for you!")
+
+@both
+async def commands(args, client, message):
+    text = "```\n"
+
+    text += "Commands for discord and minecraft" + "\n"
+    for command in list(both_commands_dict.keys()):
+        text += "    !" + command + "\n"
+    text += "\n"
+
+    text += "Commands for only discord" + "\n"
+    for command in list(discord_commands_dict.keys()):
+        text += "    !" + command + "\n"
+    text += "\n"
+
+    text += "Commands for only minecraft" + "\n"
+    for command in list(minecraft_commands_dict.keys()):
+        text += "    !" + command + "\n"
+
+    text += "```"
+
+    await client.send_message(message.channel, text)
+
+
+
+@both
+async def help(args, client, message):
+    await commands(args, client, message)
+
+@both
+async def begfordw20(args, client, message):
+    await client.send_message(message.channel, "FOR THE LAST TIME! IT'S NOT UP TO ME!")
+
+@minecraft_only
+async def add(args, client, message):
+    await client.send_message(message.channel, float(args[1]) + float(args[2]))
+
+@minecraft_only
+async def multiply(args, client, message):
+    await client.send_message(message.channel, float(args[1]) * float(args[2]))
+
+@minecraft_only
+async def subtract(args, client, message):
+    await client.send_message(message.channel, float(args[1]) - float(args[2]))
+
+@minecraft_only
+async def divide(args, client, message):
+    await client.send_message(message.channel, float(args[1]) / float(args[2]))
+
+@both
+async def output(args, client, message):
+    for i in range(len(args)):
+        print(i, args[i])
--- program.py
+++ program.py
@@ -1,6 +1,7 @@
 import discord
 import taskruns
 import secret_reader
+import commands
 
 client = discord.Client()
 
@@ -14,15 +15,16 @@ async def on_ready():
     print('Discord version number: {}'.format(discord.__version__))
     await taskruns.task_runs()
 
-
 @client.event
 async def on_message(message):
+    if not message.author.bot:
         args = message.content.lower().split(' ')
-
-        if args[0] == '!log':
-            await client.send_message(message.channel,
-                                      "Thanks, {} your message has been logged.".format(message.author))
-            await client.send_message(client.get_channel('442476383113969664'),
-                                      str(message.author) + ' ' + ' '.join(args[1:]))
+        await commands.discord_commands(args, client, message)
+        await commands.both_commands(args, client, message)
+    elif message.author.bot:
+        message.author.name = message.content.lower().split(' ')[0][1:-2]
+        in_game_args = message.content.lower().split(' ')[1:]
+        await commands.minecraft_commands(in_game_args, client, message)
+        await commands.both_commands(in_game_args, client, message)
 
 client.run(secret_reader.read_token())
