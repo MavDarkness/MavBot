1b78f2ab7f309b3fd637b900194f5d69dea48444
Johnny <john.gardner2010@gmail.com>
2018-06-08 14:24:09 -0400

    update

--- taskruns.py
+++ taskruns.py
@@ -9,3 +9,4 @@ async def task_runs():
         await asyncio.sleep(7200)
         await client.send_message(
             client.get_channel('442053138921553937'), '&5Join the discord server: https://discord.gg/UBCqM8y')
+
