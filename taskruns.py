5ee4c240de24539a6994ece93cd4ac1c2d234166
Johnny <john.gardner2010@gmail.com>
2018-06-27 15:37:24 -0400

    small changes

--- taskruns.py
+++ taskruns.py
@@ -2,9 +2,7 @@ import time
 
 
 async def task_runs(client):
-    test = True
-    while test:
-        # await time.sleep(7200)
+    while True:
+        await time.sleep(7200)
         await client.send_message(client.get_channel('442053138921553937'),
                                   'Join the discord server: https://discord.gg/UBCqM8y')
-        test = False
